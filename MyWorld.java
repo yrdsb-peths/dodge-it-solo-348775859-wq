c4cf9f14eb89ddeb7076e4a1f7986cbe30e8c527
348775859-wq <348775859@gapps.yrdsb.ca>
2026-04-17 11:04:11 -0400

    banana

--- MyWorld.java
+++ MyWorld.java
@@ -1,18 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
 {
 
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -20,5 +10,8 @@ public class MyWorld extends World
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
+        
+        Banana banana = new Banana();
+        addObject(banana, 600, 100);
     }
 }
